76cc149358a2320c4b15a69448a88e0866482d77
Adrianna Osowiecka <s13454@pjwstk.edu.pl>
2020-03-01 15:54:08 +0100

    kkkk

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //jakis komentarz
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
